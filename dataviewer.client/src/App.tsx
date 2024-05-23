3ac296a71c60c4cc7586ea32b3377a9e6472c94e
paweld139 <pawell139139@gmail.com>
2024-05-23 17:03:21 +0200

    Refactor

--- dataviewer.client/src/App.tsx
+++ dataviewer.client/src/App.tsx
@@ -20,8 +20,14 @@ interface Product {
     imagesCollection: Image[];
 }
 
+interface Column {
+    title: string;
+    field: keyof Product;
+    type?: 'image' | 'images';
+}
+
 function App() {
-    const [products, setProducts] = useState<Product[]>();
+    const [data, setData] = useState<Product[]>();
 
     useEffect(() => {
         const populateProductData = async () => {
@@ -29,50 +35,50 @@ function App() {
 
             const data = await response.json();
 
-            setProducts(data);
+            setData(data);
         }
 
         populateProductData();
     }, []);
 
-    const contents = products === undefined
+    const columns: Column[] = [
+        { title: 'Id', field: 'id' },
+        { title: 'Title', field: 'title' },
+        { title: 'Description', field: 'description' },
+        { title: 'Price', field: 'price' },
+        { title: 'Discount percentage', field: 'discountPercentage' },
+        { title: 'Rating', field: 'rating' },
+        { title: 'Stock', field: 'stock' },
+        { title: 'Brand', field: 'brand' },
+        { title: 'Category', field: 'category' },
+        { title: 'Thumbnail', field: 'thumbnail', type: 'image' },
+        { title: 'Images', field: 'imagesCollection', type: 'images' }
+    ];
+
+    const contents = data === undefined
         ? <p><em>Loading...</em></p>
         : <table className="table table-striped" aria-labelledby="tableLabel">
             <thead>
                 <tr>
-                    <th>Id</th>
-                    <th>Title</th>
-                    <th>Description</th>
-                    <th>Price</th>
-                    <th>Discount percentage</th>
-                    <th>Rating</th>
-                    <th>Stock</th>
-                    <th>Brand</th>
-                    <th>Category</th>
-                    <th>Thumbnail</th>
-                    <th>Images</th>
+                    {columns.map(column =>
+                        <th key={column.field}>{column.title}</th>
+                    )}
                 </tr>
             </thead>
             <tbody>
-                {products.map(product =>
-                    <tr key={product.id}>
-                        <td>{product.id}</td>
-                        <td>{product.title}</td>
-                        <td>{product.description}</td>
-                        <td>{product.price}</td>
-                        <td>{product.discountPercentage}</td>
-                        <td>{product.rating}</td>
-                        <td>{product.stock}</td>
-                        <td>{product.brand}</td>
-                        <td>{product.category}</td>
-                        <td>
-                            <img src={product.thumbnail} />
-                        </td>
-                        <td>
-                            <div style={{ display: 'flex' }}>
-                                {product.imagesCollection.map(image => <img key={image.id} src={image.url} />)}
-                            </div>
-                        </td>
+                {data.map(element =>
+                    <tr key={element.id}>
+                        {columns.map(column =>
+                            <td key={column.field}>
+                                {column.type === 'image' && <img src={String(element[column.field])} alt={element.title} />}
+                                {column.type === 'images' && (
+                                    <div style={{ display: 'flex' }}>
+                                        {(element[column.field] as Image[]).map((image: Image) => <img key={image.id} src={image.url} alt={element.title} />)}
+                                    </div>
+                                )}
+                                {column.type !== 'image' && column.type !== 'images' && String(element[column.field])}
+                            </td>
+                        )}
                     </tr>
                 )}
             </tbody>
